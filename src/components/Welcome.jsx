10327c937fa78d9354578dfb11c89591aae55618
Marine Massiet <massietmarine@gmail.com>
2023-09-14 10:15:54 +0200

    modif p de Welcome

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -4,7 +4,7 @@ export default function Welcome() {
   return (
     <div className="welcome">
         <h1>Clean'R services</h1>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo ut, aut deleniti non iste unde voluptatum vitae consectetur corporis magnam natus eos, sunt cumque ipsa temporibus, fugiat dolorem doloremque magni? Aperiam modi consectetur pariatur aliquid quos sint amet cum exercitationem! Consequuntur fugit aut iusto nesciunt, fuga quam labore voluptas rerum.</p>
+        <p>Clean'R Services est une société de nettoyage aussi bien pour les particuliers que pour les professionels. Nettoyage du domicile, locaux professionnels, nettoyage après chantier... Pour toute demande d'informations ou de devis, n'hésitez à nous contacter via le formulaire ci-dessous.</p>
     </div>
   )
 }
