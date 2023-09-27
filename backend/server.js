4de8f73a3b00d2dbd526d55fd2db17df3774105b
Marine Massiet <massietmarine@gmail.com>
2023-09-27 11:08:39 +0200

    modif server.js, ContactForm.jsx pour gestion formulaire et fetch

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,16 @@
-const express = require('express');
-const nodemailer = require('nodemailer');
-const bodyParser = require('body-parser');
-require ('dotenv').config(); // Chargez les variables d'environnement à partir du fichier .env
+import express from 'express';
+import nodemailer from 'nodemailer';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+import cors from 'cors';
 
 const app = express();
+
+// Utilisez cors avec une configuration pour autoriser les requêtes provenant de l'origine spécifiée
+app.use(cors({
+  origin: 'http://localhost:5173', // Remplacez par l'URL de votre site web
+  methods: 'POST', // Autorisez uniquement les requêtes POST
+}));
 
 // Middleware pour parser les données du formulaire
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -11,44 +18,47 @@ app.use(bodyParser.json());
 
 // Configuration de nodemailer pour envoyer l'e-mail
 const transporter = nodemailer.createTransport({
-    service: process.env.EMAIL_SERVICE, // Utilisation de la variable d'environnement
-    auth: {
-        user: process.env.EMAIL_USER, // Utilisation de la variable d'environnement
-        pass: process.env.EMAIL_PASSWORD // Utilisation de la variable d'environnement
-    }
+  service: process.env.EMAIL_SERVICE,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASSWORD,
+  },
 });
 
 // Gérer la requête POST lorsque le formulaire est soumis
-app.post('/envoyer-email', (req, res) => {
-    const { lastname, firstname, email, object, message } = req.body;
+app.post("/envoyer-email", (req, res) => {
+  const { lastname, firstname, email, object, message } = req.body;
 
-    // Options de l'e-mail
-    const mailOptions = {
-        from: email, // Utilisez l'e-mail de l'expéditeur spécifié dans le formulaire
-        to: 'adresse-destinataire@email.com', // Adresse e-mail du destinataire
-        subject: 'Nouveau message de formulaire de contact',
-        text: `
+  // Options de l'e-mail
+  const mailOptions = {
+    from: email, // Utilisez l'e-mail de l'expéditeur spécifié dans le formulaire
+    to: "adresse-destinataire@email.com", // Adresse e-mail du destinataire
+    subject: "Nouveau message de formulaire de contact",
+    text: `
             Nom : ${lastname}
             Prénom : ${firstname}
             E-mail : ${email}
             Objet : ${object}
             Message : ${message}
-        `
-    };
+        `,
+  };
 
-    // Envoi de l'e-mail
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.error(error);
-            res.status(500).send('Erreur lors de l\'envoi de l\'e-mail');
-        } else {
-            console.log('E-mail envoyé : ' + info.response);
-            res.status(200).send('E-mail envoyé avec succès');
-        }
-    });
+  // Envoi de l'e-mail
+  transporter.sendMail(mailOptions, (error, info) => {
+    // En cas d'erreur lors de l'envoi de l'e-mail
+    if (error) {
+      console.error(error);
+      return res
+        .status(500)
+        .json({ error: "Erreur lors de l'envoi de l'e-mail" });
+    } else {
+      console.log("E-mail envoyé : " + info.response);
+      return res.status(200).json({ message: "E-mail envoyé avec succès" });
+    }
+  });
 });
 
 const port = 3000;
 app.listen(port, () => {
-    console.log(`Serveur en cours d'exécution sur le port ${port}`);
+  console.log(`Serveur en cours d'exécution sur le port ${port}`);
 });
--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,35 +1,153 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 export default function ContactForm() {
+    const [formData, setFormData] = useState({
+        lastname: '',
+        firstname: '',
+        email: '',
+        object: 'ask-estimate',
+        message: '',
+    });
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await fetch("http://localhost:3000/envoyer-email", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(formData),
+            });
+            if (response.ok) {
+                console.log("E-mail envoyé avec succès");
+                // Réinitialisez le formulaire après l'envoi réussi si nécessaire
+                setFormData({
+                    lastname: '',
+                    firstname: '',
+                    email: '',
+                    object: 'ask-estimate',
+                    message: '',
+                });
+            } else {
+                console.error("Erreur lors de l'envoi de l'e-mail");
+            }
+        } catch (error) {
+            console.error("Erreur inattendue:", error);
+            console.error(error.stack); // Affichez la pile d'appels de l'erreur
+        }
+    };
+
+    const handleChange = (e) => {
+        // Mettez à jour les données du formulaire en fonction des changements de l'utilisateur
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
+
     return (
         <div className='contactForm'>
             <h2>Nous contacter</h2>
-            <form action="http://localhost:3000/envoyer-email" className='form'>
+            <form onSubmit={handleSubmit} className='form'>
                 <div className="lastname">
-                <label htmlFor="lastname">Nom :</label>
-                <input type="text" name='lastname' id='lastname' required/>
+                    <label htmlFor="lastname">Nom :</label>
+                    <input
+                        type="text"
+                        name='lastname'
+                        id='lastname'
+                        required
+                        value={formData.lastname}
+                        onChange={handleChange} // Gestionnaire onChange
+                    />
                 </div>
                 <div className="firstname">
-                <label htmlFor="firstname">Prénom :</label>
-                <input type="text" name='firstname' id='firstname' required/>
+                    <label htmlFor="firstname">Prénom :</label>
+                    <input
+                        type="text"
+                        name='firstname'
+                        id='firstname'
+                        required
+                        value={formData.firstname}
+                        onChange={handleChange} // Gestionnaire onChange
+                    />
                 </div>
                 <div className="email">
-                <label htmlFor="email">Mail :</label>
-                <input type="mail" name='email' id='email' required/>
+                    <label htmlFor="email">Mail :</label>
+                    <input
+                        type="email"
+                        name='email'
+                        id='email'
+                        required
+                        value={formData.email}
+                        onChange={handleChange} // Gestionnaire onChange
+                    />
                 </div>
                 <div className="object">
-                <label htmlFor="object">Objet :</label>
-                <select name="object" id="object" required>
-                    <option value="ask-estimate">Demande de devis</option>
-                    <option value="ask-infos">Demande de renseignement</option>
-                </select>
+                    <label htmlFor="object">Objet :</label>
+                    <select
+                        name="object"
+                        id="object"
+                        required
+                        value={formData.object}
+                        onChange={handleChange} // Gestionnaire onChange
+                    >
+                        <option value="ask-estimate">Demande de devis</option>
+                        <option value="ask-infos">Demande de renseignement</option>
+                    </select>
                 </div>
                 <div className="message">
-                <label htmlFor="message">Votre message :</label>
-                <textarea name='message' id='message' required/>
+                    <label htmlFor="message">Votre message :</label>
+                    <textarea
+                        name='message'
+                        id='message'
+                        required
+                        value={formData.message}
+                        onChange={handleChange} // Gestionnaire onChange
+                    />
                 </div>
-                <button>Envoyer</button>
+                <button type="submit">Envoyer</button>
             </form>
         </div>
     )
 }
+
+
+
+// export default function ContactForm() {
+//     return (
+//         <div className='contactForm'>
+//             <h2>Nous contacter</h2>
+//             <form action="http://localhost:3000/envoyer-email" className='form' method="POST">
+//                 <div className="lastname">
+//                 <label htmlFor="lastname">Nom :</label>
+//                 <input type="text" name='lastname' id='lastname' required/>
+//                 </div>
+//                 <div className="firstname">
+//                 <label htmlFor="firstname">Prénom :</label>
+//                 <input type="text" name='firstname' id='firstname' required/>
+//                 </div>
+//                 <div className="email">
+//                 <label htmlFor="email">Mail :</label>
+//                 <input type="mail" name='email' id='email' required/>
+//                 </div>
+//                 <div className="object">
+//                 <label htmlFor="object">Objet :</label>
+//                 <select name="object" id="object" required>
+//                     <option value="ask-estimate">Demande de devis</option>
+//                     <option value="ask-infos">Demande de renseignement</option>
+//                 </select>
+//                 </div>
+//                 <div className="message">
+//                 <label htmlFor="message">Votre message :</label>
+//                 <textarea name='message' id='message' required/>
+//                 </div>
+//                 <button>Envoyer</button>
+//             </form>
+//         </div>
+//     )
+// }
+
+
+
